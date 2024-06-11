8d23f93867c4d145ea02ab7897fa872608299852
@sachin.kaushik1 <sachin.kaushik@prismberry.com>
2024-06-12 00:17:17 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <h1>
-      Hello , Welcome to Next Js Tut Day 1.
+      Hello , Welcome to Next Js Tut.
     </h1>
   );
 }
